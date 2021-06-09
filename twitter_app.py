2809f2a3e332f1e15c4e71680331101d4404c809
taariqismail1 <taariqismail1@gmail.com>
2021-06-10 00:17:09 +0100

    Add files via upload

--- twitter_app.py
+++ twitter_app.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 
 # Twitter API function
 
-def get_winners(url, number):
+def get_winners_RT(url, number):
     key = 'a0AEqCf5tsBT5d3XVOiZiGbHU'
     secret = 'RrV6ciA2WNJr3IZLOyPFrL1NbkMTYXQBkq4WMZo72pschemMAB'
     auth_url = 'https://api.twitter.com/oauth2/token'
@@ -36,10 +36,36 @@ def get_winners(url, number):
     all_retweeters = list(retweeters)
     shuffle(all_retweeters)
     winners = all_retweeters[:number]
-    winners = ["@"+str(i)+"<br>" for i in winners]
+    winners = ["<a href = \"https://twitter.com/"+str(i)+"\""+"target=\"_blank\">@"+str(i)+"</a><br>" for i in winners]
 
     return(''.join(winners))
     
+def get_winners_like(url, number):
+    key = 'a0AEqCf5tsBT5d3XVOiZiGbHU'
+    secret = 'RrV6ciA2WNJr3IZLOyPFrL1NbkMTYXQBkq4WMZo72pschemMAB'
+    auth_url = 'https://api.twitter.com/oauth2/token'
+    data = {'grant_type': 'client_credentials'}
+    auth_resp = requests.post(auth_url, auth=(key, secret), data=data)
+    token = auth_resp.json()['access_token']
+    
+    tweet_id = url.split('/')[-1].split('?')[0]
+    url = 'https://api.twitter.com/2/tweets/%s/liking_users' % tweet_id
+    headers = {'Authorization': 'Bearer %s' % token}
+    likes_resp = requests.get(url, headers=headers)
+    likes = likes_resp.json()
+    
+    likes_details = likes['data']
+    # retweeters = [r['user']['screen_name'] for r in retweets]
+    
+    
+    likers = [r['username'] for r in likes_details]
+    
+    all_likers = list(likers)
+    shuffle(all_likers)
+    winners = all_likers[:number]
+    winners = ["<a href = \"https://twitter.com/"+str(i)+"\""+"target=\"_blank\">@"+str(i)+"</a><br>" for i in winners]
+
+    return(''.join(winners))
 
 
 
@@ -54,13 +80,12 @@ def submit():
     if request.method == 'POST':
         url = request.form['tweet']
         numberWinners = request.form['numberWinners']
+        comp_type = request.form['comp']
         tweet_id = url.split('/')[-1].split('?')[0]
-        if not tweet_id.isdigit():
-                return render_template('index.html', message='Invalid tweet URL')
-            
-            
-            
-        return render_template('success.html', winners = get_winners(url, int(numberWinners)))
+        if comp_type == 'retweet':
+            return render_template('success.html', winners = get_winners_RT(url, int(numberWinners)))
+        elif comp_type == 'like':
+            return render_template('success.html', winners = get_winners_like(url, int(numberWinners)))
 
 
 
